aca4d9250b321aadb9e46689f082a5ea915f9868
Maria Mir <maria-mir@hotmail.com>
2019-03-01 01:10:22 +0000

    Added templates folder, recipes.html and configured app.py

--- app.py
+++ app.py
@@ -1,11 +1,19 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, redirect, url_for, request
+from flask_pymongo import PyMongo
 
 app = Flask(__name__)
+app.config["MONGO_DBNAME"] = 'online cookbook'
+app.config["MONGO_URI"] = 'mongodb://admin:mariam83@ds125945.mlab.com:25945/recipes'
+
+mongo = PyMongo(app)
 
 @app.route('/')
-def hello():
-    return 'Hello World..'
+@app.route('/get_recipes')
+def get_recipes():
+    return render_template("recipes.html",
+    recipes=mongo.db.recipes.find())
+    
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'), 
