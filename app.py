0d49de54554f749792f8b7593df64a1e3ffbba0e
Maria Mir <maria-mir@hotmail.com>
2019-03-19 18:41:21 +0000

    refactored code and added comments in app.py

--- app.py
+++ app.py
@@ -9,16 +9,30 @@ app.config["MONGO_URI"] = 'mongodb://admin:mariam83@ds125945.mlab.com:25945/reci
 app.config['SECRET_KEY'] = os.urandom(24) 
 mongo = PyMongo(app)
 
+""" Querying MongoDB """
+recipes=mongo.db.recipes.find()
+dishes=mongo.db.dishes.find()
+cuisines=mongo.db.cuisines.find()
+allergens=mongo.db.allergens.find()
+total_recipes=recipes.count()
+
+""" Variables """
+users = mongo.db.users
+recipe =  mongo.db.recipes
+cuisine =  mongo.db.cuisines
+dish =  mongo.db.dishes
+
+""" Load user login/registration page """
 @app.route('/')
 def index():
     return render_template("index.html")
 
+""" Check data submitted via Registration form """
 @app.route('/register', methods=['POST'])
 def register():
     fullname = request.form.get('fullname')
     username = request.form.get('username')
     password = request.form.get('password')
-    users = mongo.db.users
     registered = users.find_one({'username': username})
     if registered is None:
         users.insert_one({
@@ -31,18 +45,12 @@ def register():
     success = False    
     return render_template('index.html', success = success)
         
-
+""" Check data submitted via Login form """
 @app.route('/logout', methods=['POST'])
 def login():
-    fullname = request.form.get('fullname')
     username = request.form.get('username')
     password = request.form.get('password')
-    users = mongo.db.users
-    registered = users.find_one({
-        'username': username,
-        'password': password
-    })
-    recipes=mongo.db.recipes.find()
+    registered = users.find_one({'username': username, 'password': password})
     if registered is not None:
         session['username'] = username
         login = True
@@ -50,47 +58,43 @@ def login():
     login = False    
     return render_template('index.html', login=login)
 
+""" Redirects guest users to website home page """
 @app.route('/guest_user')
 def guest_user():
     return redirect(url_for('all_recipes'))
 
+""" Logout a user by removing username from session """
 @app.route('/login')
 def logout():
     session.clear()
     return render_template('index.html')
 
-
+""" Home page displaying all uploaded recipes """
 @app.route('/all_recipes')
 def all_recipes():
-    dishes=mongo.db.dishes.find()
-    recipes=mongo.db.recipes.find()
-    total_recipes=recipes.count()
     return render_template("home.html", recipes=recipes, dishes=dishes, total_recipes=total_recipes)
-    
+ 
+""" Displays detail view of a recipe """    
 @app.route('/the_recipe/<recipe_id>/<recipe_title>')
 def the_recipe(recipe_id, recipe_title):
-    recipes=mongo.db.recipes
     return render_template("recipe.html",
                         recipe = recipes.find_one({'_id': ObjectId(recipe_id),'recipe_title': recipe_title}))
-    
+
+""" Display form to add a recipe """ 
 @app.route('/add_recipe')
 def add_recipe():
-    return render_template("addrecipe.html",
-    cuisines=mongo.db.cuisines.find(),
-    dishes=mongo.db.dishes.find(),
-    allergens=mongo.db.allergens.find())
+    return render_template("addrecipe.html", cuisines=cuisines, dishes=dishes, allergens=allergens)
 
+""" Display form to edit the recipe """
 @app.route('/edit_recipe/<recipe_id>')
 def edit_recipe(recipe_id):
     return render_template('editrecipe.html',  
                         recipe =  mongo.db.recipes.find_one({"_id": ObjectId(recipe_id)}),
-                        cuisines = mongo.db.cuisines.find(),
-                        dishes = mongo.db.dishes.find(),
-                        allergens = mongo.db.allergens.find())
+                        cuisines = cuisines, dishes = dishes, allergens = allergens)
 
+""" Send form data to update recipe in MongoDB """
 @app.route('/update_recipe/<recipe_id>', methods=["POST"])
 def update_recipe(recipe_id):
-    recipe =  mongo.db.recipes
     recipe.update( {'_id': ObjectId(recipe_id)},
     {
         'recipe_author_name': session['username'],
@@ -109,7 +113,7 @@ def update_recipe(recipe_id):
     })
     return redirect(url_for('all_recipes'))
   
-   
+""" Add new recipe data to MongoDB """   
 @app.route('/insert_recipe', methods=['POST'])
 def insert_recipe():
     doc ={'recipe_author_name': session['username']}
@@ -127,99 +131,89 @@ def insert_recipe():
         else:
             doc[k]= v  
 
-    recipe = doc
-    recipes=mongo.db.recipes 
-    recipes.insert_one(recipe)
+    new_recipe = doc
+    recipes.insert_one(new_recipe)
     return redirect(url_for('all_recipes'))
 
+""" Removes a recipe from MongoDB """
 @app.route('/delete_recipe/<recipe_id>')
 def delete_recipe(recipe_id):
-    recipe =  mongo.db.recipes
     recipe.remove({'_id': ObjectId(recipe_id)})
     return redirect(url_for('all_recipes')) 
 
+""" Displays all cuisines in MongoDB """
 @app.route('/all_cuisines')
 def all_cuisines():
-    all_cuisines=mongo.db.cuisines.find()
-    return render_template("allcuisines.html", cuisines=all_cuisines)
+    return render_template("allcuisines.html", cuisines=cuisines)
 
-
+""" Displays form to add new cuisine """
 @app.route('/add_cuisine')
 def add_cuisine():
-    return render_template("addcuisine.html",
-    cuisines=mongo.db.cuisines.find())
+    return render_template("addcuisine.html", cuisines=cuisines)
 
+""" Adds new cuisine to MongoDB """
 @app.route('/insert_cuisine', methods=['POST'])
 def insert_cuisine():
-    cuisines=mongo.db.cuisines
     cuisine = request.form.to_dict()
     cuisines.insert_one(cuisine)
     return redirect(url_for('all_cuisines'))
-    
+ 
+""" Displays form to edit cuisine """    
 @app.route('/edit_cuisine/<cuisine_id>')
 def edit_cuisine(cuisine_id):
     cuisine=mongo.db.cuisines.find_one({"_id": ObjectId(cuisine_id)})
-    return render_template('editcuisine.html',
-                            cuisine=cuisine)    
- 
+    return render_template('editcuisine.html', cuisine=cuisine)    
+
+""" Send form data to update cuisine in MongoDB """ 
 @app.route('/update_cuisine/<cuisine_id>', methods=["POST"])
 def update_cuisine(cuisine_id):
-    cuisine =  mongo.db.cuisines
-    cuisine.update( 
-        {'_id': ObjectId(cuisine_id)},
-        {'cuisine_name': request.form.get('cuisine_name')}
-        )
+    cuisine.update({'_id': ObjectId(cuisine_id)},
+                   {'cuisine_name': request.form.get('cuisine_name')})
     return redirect(url_for('all_cuisines'))  
 
+""" Removes a cuisine from MongoDB """
 @app.route('/delete_cuisine/<cuisine_id>')
 def delete_cuisine(cuisine_id):
-    cuisine =  mongo.db.cuisines
     cuisine.remove({'_id': ObjectId(cuisine_id)})
     return redirect(url_for('all_cuisines'))       
- 
+
+""" Displays all dishes in MongoDB """ 
 @app.route('/all_dishes')
 def all_dishes():
-    all_dishes=mongo.db.dishes.find()
-    return render_template("alldishes.html", dishes=all_dishes)
+    return render_template("alldishes.html", dishes=dishes)
 
-
+""" Displays form to add a new dish """
 @app.route('/add_dish')
 def add_dish():
-    return render_template("adddish.html",
-    dishes=mongo.db.dishes.find())
+    return render_template("adddish.html", dishes=dishes)
 
+""" Adds a new dish to MongoDB """
 @app.route('/insert_dish', methods=['POST'])
 def insert_dish():
-    dishes=mongo.db.dishes
     dish = request.form.to_dict()
     dishes.insert_one(dish)
     return redirect(url_for('all_dishes'))
-    
+ 
+""" Displays form to edit a dish """    
 @app.route('/edit_dish/<dish_id>')
 def edit_dish(dish_id):
-    dish=mongo.db.dishes.find_one({"_id": ObjectId(dish_id)})
-    return render_template('editdish.html',
-                            dish=dish)    
- 
+    dish.find_one({"_id": ObjectId(dish_id)})
+    return render_template('editdish.html', dish=dish)    
+
+""" Send form data to update the dish in MongoDB """ 
 @app.route('/update_dish/<dish_id>', methods=["POST"])
 def update_dish(dish_id):
-    dish =  mongo.db.dishes
-    dish.update( 
-        {'_id': ObjectId(dish_id)},
-        {'dish_type': request.form.get('dish_type')}
-        )
+    dish.update({'_id': ObjectId(dish_id)},
+                {'dish_type': request.form.get('dish_type')})
     return redirect(url_for('all_dishes'))  
 
+""" Remove the dish in MongoDB """ 
 @app.route('/delete_dish/<dish_id>')
 def delete_dish(dish_id):
-    dish =  mongo.db.dishes
     dish.remove({'_id': ObjectId(dish_id)})
     return redirect(url_for('all_dishes')) 
 
-@app.route('/search/<dish_type>')
-def search(dish_type):
-    dish = mongo.db.dishes
-    dish.find({'dish_type':'Soup'})
+
     
   
 
